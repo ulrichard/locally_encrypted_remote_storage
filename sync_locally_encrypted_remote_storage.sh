16a9cfe9a91b89a95748afd32288bb6919e76073
Richard Ulrich <richi@paraeasy.ch>
2013-09-26 20:26:21 +0200

    first fully working version (including creation)

--- sync_locally_encrypted_remote_storage.sh
+++ sync_locally_encrypted_remote_storage.sh
@@ -21,40 +21,45 @@ sshfs ${extUser}@${extHOST}:${extPath} /tmp/${extHOST}_hd/A -o allow_root
 
 if [ ! -e /tmp/${extHOST}_hd/A/${extImgName}.img ]; then
 	# prepare the disk image on the remote machine
-	ssh ${extUser}@${extHOST} "dd if=/dev/urandom of=${extPath}/${extImgName}.img bs=1M count=${imageSize}"
+	ssh ${extUser}@${extHOST} "(dd if=/dev/urandom of=${extPath}/${extImgName}.img bs=1M count=${imageSize}; chmod 700 ${extPath}/${extImgName}.img)"
 	sudo modprobe dm-crypt
-	sudo modprobe aes
-	sudo modprobe sha256
+#	sudo modprobe aes
+#	sudo modprobe sha256
 	# prepare a key file and encrypt it
 	dd if=/dev/urandom | tr -d '\n' | dd bs=1 count=64 of=key_${extHOST}_${extImgName}.txt
 	gpg -e key_${extHOST}_${extImgName}.txt
-	cryptsetup -c aes-xts-plain -s 512 luksFormat /tmp/${extHOST}_hd/A/${extImgName}.img key_${extHOST}_${extImgName}.txt
-	shred key_${extHOST}_${extImgName}.txt
-	rm key_${extHOST}_${extImgName}.txt
+	sudo cryptsetup -c aes-xts-plain -s 512 luksFormat /tmp/${extHOST}_hd/A/${extImgName}.img key_${extHOST}_${extImgName}.txt
+	
+	sudo losetup /dev/${loopDevice} /tmp/${extHOST}_hd/A/${extImgName}.img
+
+	sudo cryptsetup luksOpen /dev/${loopDevice} ${extHOST} < key_${extHOST}_${extImgName}.txt
+
+	sudo mke2fs /dev/mapper/${extHOST}
+else
+	sudo losetup /dev/${loopDevice} /tmp/${extHOST}_hd/A/${extImgName}.img
+
+	gpg -d key_${extHOST}_${extImgName}.txt.gpg > key_${extHOST}_${extImgName}.txt
+	sudo cryptsetup luksOpen /dev/${loopDevice} ${extHOST} < key_${extHOST}_${extImgName}.txt
 fi
 
-#sudo mkdir -p /tmp/${extHOST}_hd/B
-#sudo chmod 700 /tmp/${extHOST}_hd/B
-sudo losetup /dev/${loopDevice} /tmp/${extHOST}_hd/A/${extImgName}.img
-
-gpg -d key_${extHOST}_${extImgName}.txt.gpg > key_${extHOST}_${extImgName}.txt
-sudo cryptsetup luksOpen /dev/${loopDevice} ${extHOST} < key_${extHOST}_${extImgName}.txt
 shred key_${extHOST}_${extImgName}.txt
 rm key_${extHOST}_${extImgName}.txt
 
-#sudo mke2fs /dev/mapper/${extHOST}
 sudo mkdir -p /tmp/${extHOST}_hd/C
 sudo chmod 700 /tmp/${extHOST}_hd/C
 sudo mount /dev/mapper/${extHOST} /tmp/${extHOST}_hd/C
 echo "the locally encrypted remote storage is now mounted at /tmp/${extHOST}_hd/C"
 
 if [ ! -d /tmp/${extHOST}_hd/C/${extImgName}.git ]; then
-	mkdir -p /tmp/${extHOST}_hd/C/${extImgName}.git
+	sudo mkdir -p /tmp/${extHOST}_hd/C/${extImgName}.git
+	sudo chown ${extUser} /tmp/${extHOST}_hd/C/${extImgName}.git
 	(cd /tmp/${extHOST}_hd/C/${extImgName}.git; git init --bare)
+else
+	git pull /tmp/${extHOST}_hd/C/${extImgName}.git master
 fi
 
-git pull /tmp/${extHOST}_hd/C/${extImgName}.git master
 git push /tmp/${extHOST}_hd/C/${extImgName}.git master
+
 read -p "Press [enter] to close the storage again ..."
 
 sudo sync
